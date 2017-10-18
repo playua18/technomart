22f040f69a0ca9ff704d7a05380b848bb2d6947f
playua18 <playua18@gmail.net>
2017-10-18 19:02:33 +0300

    Добавлен querySelectorAll метод в js

--- js/modal-item.js
+++ js/modal-item.js
@@ -1,18 +1,29 @@
-var linkitem = document.querySelector('.action-btn-buy');
+var linkitem = document.querySelectorAll('.action-btn-buy');
 var modalitem = document.querySelector('.modal-item-added');
+var modalshow = document.querySelector('.modal-item-added-show');
 var modalclose = document.querySelector('.modal-item-close');
-linkitem.addEventListener('click', function (event) {
-  event.preventDefault();
-  modalitem.classList.add('modal-item-added-show');
-});
-modalclose.addEventListener('click', function (event) {
+var shopping = document.querySelector('.btn-continue-shopping');
+
+
+for (var i = 0; i < linkitem.length; i++) {
+  linkitem[i].addEventListener('click', function(event) {
+    event.preventDefault();
+    modalitem.classList.add('modal-item-added-show');
+  });
+};
+
+modalclose.addEventListener('click', function(event) {
   event.preventDefault();
   modalitem.classList.remove('modal-item-added-show');
 });
-window.addEventListener('keydown', function (event) {
+window.addEventListener('keydown', function(event) {
   if (event.keyCode === 27) {
     if (modalitem.classList.contains('modal-item-added-show')) {
       modalitem.classList.remove('modal-item-added-show');
     }
   }
+});
+shopping.addEventListener('click', function(event) {
+  event.preventDefault();
+  modalitem.classList.remove('modal-item-added-show');
 });
